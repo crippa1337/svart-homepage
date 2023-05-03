b148e67388360ac626372abb8e58775950cf1b80
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-05-03 10:10:14 +0200

    forgot to push, uhhhhh

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+// Navbar transparency fade effect
+
+// Fetch the object with a query selector
+// Query selectors return the first object matching the query
+// and since there's only one '.nav__links', it'll always be the right one.
+const nav = document.querySelector('.nav__links');
+
+console.log(nav)
