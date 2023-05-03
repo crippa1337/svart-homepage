78121b466adfccbb215f307f2dd3770cfe844833
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-05-03 11:07:48 +0200

    comments, download page init and static file

--- assets/index.js
+++ assets/index.js
@@ -7,15 +7,18 @@ const nav = document.querySelector('.nav__links');
 let isTransparent = true;
 let verticalPosition = 0;
 
+// Event listener "scroll" detects when the user
+// scrolls in the window and executes the code inside.
 addEventListener("scroll", () => {
+    // Fetch the current vertical window position. (0 being at the top)
     verticalPosition = window.scrollY;
 
+    // If we're not transparent and at the top, make it transparent.
     if (!isTransparent && verticalPosition === 0) {
         nav.classList.add('nav-transparent');
         nav.classList.remove('nav-opaque');
         isTransparent = true;
-        // If we're transparent but not at the top, make it
-        // opaque.
+        // If we're transparent but not at the top, make it opaque.
     } else if (isTransparent && verticalPosition > 0) {
         nav.classList.remove('nav-transparent');
         nav.classList.add('nav-opaque');
