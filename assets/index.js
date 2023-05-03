38570b578377e29a4ccfbb71366ab0636a31370b
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-05-03 10:32:41 +0200

    use an event listener instead

--- assets/index.js
+++ assets/index.js
@@ -5,25 +5,20 @@
 // and since there's only one '.nav__links', it'll always be the right one.
 const nav = document.querySelector('.nav__links');
 let isTransparent = true;
+let verticalPosition = 0;
 
-setInterval(() => {
-    // Get scroll position, how far down the window
-    // the user has scrolled. Is 0 at the top.
-    const scrollLength = window.scrollY;
+addEventListener("scroll", () => {
+    verticalPosition = window.scrollY;
 
-    // If we're not currently transparent, but at the top
-    // we make it transparent.
-    if (!isTransparent && scrollLength === 0) {
+    if (!isTransparent && verticalPosition === 0) {
         nav.classList.add('nav-transparent');
         nav.classList.remove('nav-opaque');
         isTransparent = true;
         // If we're transparent but not at the top, make it
         // opaque.
-    } else if (isTransparent && scrollLength > 0) {
+    } else if (isTransparent && verticalPosition > 0) {
         nav.classList.remove('nav-transparent');
         nav.classList.add('nav-opaque');
         isTransparent = false;
     }
-
-    // Check for scrolling once every 33ms (30 times a second)
-}, 33);
+});
