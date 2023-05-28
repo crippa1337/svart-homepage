123f5480b8ab79e72cc94390add61dbe54233b74
crippa1337 <casdcasd5@gmail.com>
2023-05-28 18:55:58 +0200

    sussy fix 2

--- assets/scripts/static.js
+++ assets/scripts/static.js
@@ -3,11 +3,12 @@
 
 // Fetch all the links
 const allLinks = document.querySelectorAll('a');
+const origin = window.location.origin;
 
 // Loop through the links
 for (const link of allLinks) {
     // If it is an external link
-    if (link.href.startsWith('https')) {
+    if (link.href.startsWith('https') && !link.href.startsWith(origin)) {
         // Set the target to '_blank' (which opens a new tab instead)
         link.target = '_blank';
     }
